dad5f183032d32ff2ba251a092905cbc7091443f
shree007 <shreeprakashagrahari05@gmail.com>
2023-08-30 23:45:33 +0530

    initial commit

--- docker-repository/main.go
+++ docker-repository/main.go
@@ -3,10 +3,17 @@ package main
 
 import(
 	"fmt"
+	"log"
+	//"log/slog" available: 1.21 version, Learning curve https://betterstack.com/community/guides/logging/logging-in-go/
 )
+
+func awsCreateEcrRepository(){
+	log.Printf("hello, world")
+}
 
 
 
 func main(){
 	fmt.Println("create docker repository")
+	awsCreateEcrRepository()
 }
