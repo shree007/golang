b475fdbb50d15f0045ece3dffa3fb0aff6877816
shree007 <shreeprakashagrahari05@gmail.com>
2024-11-07 10:39:31 +0530

    HELM-11 download indexfile from jfrog artifactory

--- automated-helm-charts/main.go
+++ automated-helm-charts/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -21,32 +22,33 @@ const (
 	chartBasePath    = "helm/charts"
 	packagePath      = "temp-helm-storage"
 	indexFilePath    = "temp-helm-storage/index.yaml"
-	jfrogArtifactURL = "https://linkinpark.jfrog.io/artifactory"
-	jfrogRepoName    = "linkinpark-helm"
+	jfrogArtifactURL = "https://linkinpark.jfrog.io/artifactory/"
+	jfrogRepoName    = "linkinpark-helmchart-helm-local"
 )
 
 /*
 Cleanup index file old packaged charts and before starting
 */
 func init() {
-	matches, err := filepath.Glob(filepath.Join(packagePath, "*.tgz"))
-	if err != nil {
-		log.Fatalf("Error finding .tgz files: %v", err)
+	if _, err := os.Stat(packagePath); os.IsNotExist(err) {
+		log.Errorf("Directory does not exists, nothing to remove %v", err)
 	}
 
-	for _, match := range matches {
-		if err := os.Remove(match); err != nil {
-			log.Printf("Failed to remove file %s: %v", match, err)
-		}
-	}
-	if err := os.Truncate(indexFilePath, 0); err != nil {
-		log.Fatalf("Failed to truncate file: %v", err)
+	if err := os.RemoveAll(packagePath); err != nil {
+		log.Errorf("Failed to remove directory %v", err)
+	} else {
+		log.Info("Directory has been removed")
 	}
 }
 
 func main() {
 	log.Info("Starting Helm chart processing")
-	index := createIndexFile(indexFilePath)
+
+	if err := downloadIndexFile(); err != nil {
+		log.Fatalf("Failed to Download Index file %v", err)
+	}
+
+	index := manageIndexFile(indexFilePath)
 
 	entries, err := os.ReadDir(chartBasePath)
 	if err != nil {
@@ -96,7 +98,7 @@ func loadingChart(charPath string) (*chart.Chart, error) {
 	return loader.Load(charPath)
 }
 
-func createIndexFile(indexFilePath string) *repo.IndexFile {
+func manageIndexFile(indexFilePath string) *repo.IndexFile {
 	index := repo.NewIndexFile()
 
 	if _, err := os.Stat(indexFilePath); err == nil {
@@ -167,11 +169,15 @@ func lintChart(chart *chart.Chart) error {
 
 func addToIndex(chart *chart.Chart, chartURL string, index *repo.IndexFile) {
 	if existingVersions, ok := index.Entries[chart.Metadata.Name]; ok {
+		already_exists_version := false
 		for _, v := range existingVersions {
 			if v.Version == chart.Metadata.Version {
-				log.Infof("Chart %s version %s already exists in the index, skipping", chart.Metadata.Name, chart.Metadata.Version)
-				return
+				already_exists_version = true
+				break
 			}
+		}
+		if already_exists_version {
+			log.Infof("Chart %s version %s already exists in the index, skipping", chart.Metadata.Name, chart.Metadata.Version)
 		}
 	}
 	index.MustAdd(chart.Metadata, chartURL, " ", " ")
@@ -189,7 +195,7 @@ func packageChart(chart *chart.Chart) string {
 		return ""
 	}
 	log.Infof("Packaged chart %s to %s", chart.Name(), pkgPath)
-	return fmt.Sprintf("%s/%s-%s.tgz", packagePath, chart.Metadata.Name, chart.Metadata.Version)
+	return fmt.Sprintf("%s%s-%s.tgz", packagePath, chart.Metadata.Name, chart.Metadata.Version)
 }
 
 func uploadToJfrogArtifactory() {
@@ -207,7 +213,7 @@ func uploadToJfrogArtifactory() {
 		}
 		defer file.Close()
 
-		uploadURL := fmt.Sprintf("%s/%s/%s", jfrogArtifactURL, jfrogRepoName, filepath.Base(chartPath))
+		uploadURL := fmt.Sprintf("%s%s/%s", jfrogArtifactURL, jfrogRepoName, filepath.Base(chartPath))
 		request, err := http.NewRequest("PUT", uploadURL, file)
 		if err != nil {
 			log.Errorf("Error whilst creating request %v", err)
@@ -228,4 +234,42 @@ func uploadToJfrogArtifactory() {
 
 		fmt.Printf("Uploaded %s successfully to %s\n", chartPath, uploadURL)
 	}
+}
+
+func downloadIndexFile() error {
+	jfrogUploadAPIKey := os.Getenv("jfrog_upload_api_key")
+	JFROGindexFileUrl := jfrogArtifactURL + jfrogRepoName + "/index.yaml"
+	log.Infof("Index file URL is %s", JFROGindexFileUrl)
+
+	request, err := http.NewRequest("GET", JFROGindexFileUrl, nil)
+	if err != nil {
+		return fmt.Errorf("Request is failed %v", err)
+	}
+	request.Header.Set("Content-Type", "application/octet-stream")
+	request.Header.Set("Authorization", "Bearer "+jfrogUploadAPIKey)
+
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return fmt.Errorf("Failed to download Index file %v", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error while download index.yaml, Status Error Code: %d", response.StatusCode)
+	}
+
+	if err := os.MkdirAll(filepath.Dir(indexFilePath), os.ModePerm); err != nil {
+		return fmt.Errorf("Failed to create directory for index file %v", err)
+	}
+
+	out, err := os.Create(indexFilePath)
+
+	if err != nil {
+		return fmt.Errorf("failed to create index file: %v", err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, response.Body)
+	return err
 }
