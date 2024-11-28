4fc82231bdaeb4a4bdd22db9955f1ce51617944d
shree007 <shreeprakashagrahari05@gmail.com>
2024-11-29 01:45:26 +0530

    Nov-28 merge two indexfile into third one

---
+++ automated-helm-charts/merge-index-file/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// https://zhwt.github.io/yaml-to-go/
+type HelmIndex struct {
+	ServerInfo  interface{}             `yaml:"serverinfo"`
+	APIVersion  string                  `yaml:"apiversion"`
+	Generated   time.Time               `yaml:"generated"`
+	Entries     map[string][]ChartEntry `yaml:"entries"`
+	PublicKeys  []interface{}           `yaml:"publickeys"`
+	Annotations map[string]interface{}  `yaml:"annotations"`
+}
+
+type ChartEntry struct {
+	Metadata               ChartMetadata `yaml:"metadata"`
+	URLs                   []string      `yaml:"urls"`
+	Created                time.Time     `yaml:"created"`
+	Removed                bool          `yaml:"removed"`
+	Digest                 string        `yaml:"digest"`
+	ChecksumDeprecate      string        `yaml:"checksumdeprecated"`
+	EngineDeprecate        string        `yaml:"enginedeprecated"`
+	TillerVersionDeprecate string        `yaml:"tillerversiondeprecated"`
+	URLDeprecate           string        `yaml:"urldeprecated"`
+}
+
+type ChartMetadata struct {
+	Name         string                 `yaml:"name"`
+	Home         string                 `yaml:"home"`
+	Sources      []string               `yaml:"sources"`
+	Version      string                 `yaml:"version"`
+	Description  string                 `yaml:"description"`
+	Keywords     []string               `yaml:"keywords"`
+	Maintainers  []Maintainer           `yaml:"maintainers"`
+	Icon         string                 `yaml:"icon"`
+	APIVersion   string                 `yaml:"apiversion"`
+	Condition    string                 `yaml:"condition"`
+	Tags         string                 `yaml:"tags"`
+	AppVersion   string                 `yaml:"appversion"`
+	Deprecated   bool                   `yaml:"deprecated"`
+	Annotations  map[string]interface{} `yaml:"annotations"`
+	KubeVersion  string                 `yaml:"kubeversion"`
+	Dependencies []string               `yaml:"dependencies"`
+	Type         string                 `yaml:"type"`
+}
+
+type Maintainer struct {
+	Name  string `yaml:"name"`
+	Email string `yaml:"email"`
+	URL   string `yaml:"url"`
+}
+
+func main() {
+	fmt.Println("Read yaml file...")
+}
