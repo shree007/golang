3232efe679cff36765235be9e0d04402a5d31fed
shree007 <shreeprakashagrahari05@gmail.com>
2024-12-02 01:06:49 +0530

    Dec-1 Marshal and Unmarshal for json

--- learn-it/marshal-unmarshal/main.go
+++ learn-it/marshal-unmarshal/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -22,6 +23,42 @@ type Person struct {
 func main() {
 	marshalYaml()
 	unmashalToYaml()
+
+	marshalJson()
+	unmarshalJson()
+}
+func marshalJson() {
+	fmt.Println("==========Marshal person struct inJSON==========")
+	person := Person{
+		Name:    "shreeprakash",
+		Age:     32,
+		Hobbies: []string{"books", "movies", "badminton"},
+	}
+	jsonData, err := json.MarshalIndent(person, "", " ")
+	if err != nil {
+		log.Fatalf("Error while marshal to json")
+	}
+	fmt.Println("JSON Data:", string(jsonData))
+	err = os.WriteFile("person.json", jsonData, 0644)
+	if err != nil {
+		log.Fatalf("Error while writing struct into person.json")
+	}
+	fmt.Println("Person struct has been written into person.json")
+}
+
+func unmarshalJson() {
+	fmt.Println("==========UNMarshal person struct inJSON==========")
+	readJsonData, err := os.ReadFile("person.json")
+	if err != nil {
+		log.Fatalf("Error while reading json file person.json")
+	}
+	var person Person
+	err = json.Unmarshal(readJsonData, &person)
+	if err != nil {
+		log.Fatalf("Error while unMarshal %v", err)
+	}
+	fmt.Println("Unmarshaled Struct:\n", person)
+
 }
 func marshalYaml() {
 	fmt.Println("==========Marshal person struct==========")
