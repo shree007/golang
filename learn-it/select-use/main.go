c6c0f7859082e151b11f9650784c047ac1c63c16
Shree Prakash <shreeprakashagrahari05@gmail.com>
2025-01-05 01:21:05 +0530

    jan-05 learn select (#61)

--- learn-it/select-use/main.go
+++ learn-it/select-use/main.go
@@ -6,6 +6,37 @@ import (
 )
 
 func main() {
+	simpleUseSelect()
+	nonBlockingCommunication()
+	infiniteLoop()
+}
+
+func infiniteLoop() {
+	ticker := time.NewTicker(1 * time.Second)
+	timeout := time.After(5 * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("Tick...")
+		case <-timeout:
+			fmt.Println("Time's up!")
+			return
+		}
+	}
+}
+
+func nonBlockingCommunication() {
+	ch := make(chan string)
+	select {
+	case msg := <-ch:
+		fmt.Println("Received:", msg)
+	default:
+		fmt.Println("No data received")
+	}
+}
+
+func simpleUseSelect() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
