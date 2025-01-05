73fd96248bcf3d87d5fc2ebc5815a2c1758d8148
shree007 <shreeprakashagrahari05@gmail.com>
2025-01-05 23:09:14 +0530

    jan-05 learn errors

--- learn-it/errors-handling/main.go
+++ learn-it/errors-handling/main.go
@@ -5,9 +5,52 @@ import (
 	"fmt"
 )
 
+var ErrorNotFound = errors.New("Not Found")
+
 func main() {
 	basicErrorHandling()
 	customErrorHandling()
+	errorsIs()
+	errorsAs()
+}
+
+type MyCustomError struct {
+	Message string
+}
+
+func (e *MyCustomError) Error() string {
+	return e.Message
+}
+
+func triggerError() error {
+	return &MyCustomError{Message: "Something went wrong"}
+}
+
+func errorsAs() {
+	err := triggerError()
+	var customErr *MyCustomError
+
+	if errors.As(err, &customErr) {
+		fmt.Println("Caught a custom error:", customErr.Message)
+	} else {
+		fmt.Println("Some other error occurred")
+	}
+}
+
+func findUser(id int) error {
+	if id == 0 {
+		return ErrorNotFound
+	}
+	return nil
+}
+
+func errorsIs() {
+	err := findUser(0)
+	if errors.Is(err, ErrorNotFound) {
+		fmt.Println("User Not Found")
+	} else {
+		fmt.Println("User Found")
+	}
 }
 
 func customErrorHandling() {
