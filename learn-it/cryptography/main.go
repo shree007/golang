66b28188496b7861eeb246615aace9c329d4c803
shree007 <shreeprakashagrahari05@gmail.com>
2025-01-09 01:25:27 +0530

    jan-09 hash file

--- learn-it/cryptography/main.go
+++ learn-it/cryptography/main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	hashToString()
+	hashFile()
+}
+
+func hashFile() {
+	file, err := os.Open("example.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		fmt.Println("Error hashing file:", err)
+		return
+	}
+	hashSum := hasher.Sum(nil)
+	fmt.Printf("File SHA-256 Hash: %x\n", hashSum)
+
 }
 
 func hashToString() {
