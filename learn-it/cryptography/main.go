09deb226a6cc9b3b47dfb34a0e6263354c967185
Shree Prakash <shreeprakashagrahari05@gmail.com>
2025-01-09 22:07:44 +0530

    jan-09 hash file (#66)
    * jan-09 hash file

--- learn-it/cryptography/main.go
+++ learn-it/cryptography/main.go
@@ -3,10 +3,46 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	hashToString()
+	hashFile()
+	comparingHash()
+}
+
+func comparingHash() {
+	data1 := "hello"
+	data2 := "world"
+
+	hash1 := sha256.Sum256([]byte(data1))
+	hash2 := sha256.Sum256([]byte(data2))
+
+	if hash1 == hash2 {
+		fmt.Println("Hashes matched")
+	} else {
+		fmt.Println("Hashes unmatched")
+	}
+}
+
+func hashFile() {
+	file, err := os.Open("example.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		fmt.Println("Error hashing file:", err)
+		return
+	}
+	hashSum := hasher.Sum(nil)
+	fmt.Printf("File SHA-256 Hash: %x\n", hashSum)
+
 }
 
 func hashToString() {
