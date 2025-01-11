8e35850cd2f459d779cbfc5d8deab0312187282c
shree007 <shreeprakashagrahari05@gmail.com>
2025-01-11 22:28:03 +0530

    jan-10 os module

--- learn-it/os-operations/main.go
+++ learn-it/os-operations/main.go
@@ -7,9 +7,16 @@ import (
 
 func main() {
 	pathJoin()
+	isDir()
 }
 
 func pathJoin() {
 	completePath := filepath.Join("dir1", "dir2", "filename.txt")
 	fmt.Println(completePath)
+}
+
+func isDir() {
+	p := "dir1/dir2/filename.txt"
+	fmt.Println("except last element in path", filepath.Dir(p))
+	fmt.Println("last element of path", filepath.Base(p))
 }
